9125a4bfd0621d16e2adfc433df746ce41be77ed
Arnaud BODEL <contact@osw3.net>
2021-04-15 15:05:38 +0200

    -

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class TaskType extends AbstractType
 {
@@ -16,13 +17,56 @@ class TaskType extends AbstractType
 
             // Name
             ->add('name', TextType::class, [
+
+                // Label
+                // --
+
                 // Texte de la balise <label>
                 'label' => "Entrer le nom de la tâche",
 
                 // Modification des attributs de la balise <label>
                 'label_attr' => [
                     'class' => "my-custom-class"
-                ]
+                ],
+
+
+                // Input
+                // --
+
+                // Rendre le champ obligatoire, ou non..
+                'required' => true,
+
+                // Modification des attibuts du champ html
+                'attr' => [
+                    // Ajouter des classes sur le champ html
+                    // 'class' => "",
+
+                    // Ajoute le Placeholder
+                    'placeholder' => "Saisir votre tâche !!!"
+                ],
+
+
+                // Helper
+                // --
+
+                // Ajoute un texte d'aide pour l'utilisateur
+                'help' => "Ceci est un texte d'aide pour l'utilisateur",
+
+                // Modifie les attributs du Helper
+                'help_attr' => [
+                    // 'class' => "CLASS-FOR-MY-HELPER"
+                ],
+
+
+                // Contraintes / Controle de formulaire
+                // --
+
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Le champ est obligatoire"
+                    ])
+                ],
+
             ])
 
             // Create At
